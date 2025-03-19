91e958624b0f6940c3846c42e0f0d267521cfd72
HuashuZhan <zhanhuashu123@gmail.com>
2025-03-19 01:22:25 +0100

    modify tooltip

--- components/plasmic/saa_s_website/PlasmicUser.jsx
+++ components/plasmic/saa_s_website/PlasmicUser.jsx
@@ -494,7 +494,12 @@ function PlasmicUser__RenderFunc(props) {
                   </span>
                   {/* tooltip*/}
                   <div className={`${sty["tooltip"]} ${showInfo ? sty["show"] : ""}`}>
-                    Select different variables to influence the accessibility analysis.
+                    <p>Select variables and set the speed penalty between 0 and 1:</p>
+                    <ul>
+                      <p><strong>0</strong>: Completely inaccessible</p>
+                      <p><strong>1</strong>: Fully comfortable</p>
+                      <p>Lower values = Greater barrier</p>
+                    </ul>
                   </div>
                 </div>
 
