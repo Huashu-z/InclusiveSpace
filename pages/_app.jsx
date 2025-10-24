d3ab119d0fa351928f9df6da6b2c00235cc57fc9
HuashuZhan <zhanhuashu123@gmail.com>
2025-10-24 11:23:16 +0200

    data tooltip, favicon

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -6,9 +6,20 @@ import { appWithTranslation } from "next-i18next";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <PlasmicRootProvider Head={Head} Link={Link}>
-      <Component {...pageProps} />
-    </PlasmicRootProvider>
+    <>
+      <Head>
+        <title>CAT – Comfort-based Accessibility Tool</title>
+        <link rel="icon" type="image/png" href="/favicon.png" />
+
+        {/* SEO and mobile support */}
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="Explore comfort-based accessibility maps for inclusive urban design." />
+      </Head>
+
+      <PlasmicRootProvider Head={Head} Link={Link}>
+        <Component {...pageProps} />
+      </PlasmicRootProvider>
+    </>
   );
 }
 
