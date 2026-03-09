65d9740f9817dd93b4f3495c27d52014c5b5a7d8
HuashuZhan <zhanhuashu123@gmail.com>
2026-03-09 10:41:23 +0100

    favicon, methodology

--- components/plasmic/saa_s_website/PlasmicMethodology.jsx
+++ components/plasmic/saa_s_website/PlasmicMethodology.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import sty from "./PlasmicLanding.module.css";
+import { useTranslation } from "next-i18next";
 
 export default function Methodology() {
+
+    const { t } = useTranslation("common");
+
   return (
     <div>
       {/* Page title */}
@@ -123,6 +127,133 @@ export default function Methodology() {
                 <div className={sty.methodologyDescTitle}>8. xxx</div>
                 <div className={sty.methodologyDescText}>
                   xxx
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* === Section 5 === */}
+      <section className={sty.toolDetailsSection}>
+        <h1 className={sty.toolDetailsTitle}>5. What do you get?</h1>
+
+        <div className={sty.toolDetailsGrid}>
+          <div className={sty.stickerCard}>
+            <h2 className={sty.toolDetailsTitle}>CAT Results</h2>
+            <div className={sty.toolDetailsText}>
+              <p>
+                xxxxxx
+              </p>
+            </div>
+          </div>
+
+          <div className={sty.stickerCard}>
+            <h2 className={sty.toolDetailsTitle}>Limitations and Digital Accesibility Transparency</h2>
+            <div className={sty.toolDetailsText}>
+              <p>
+                xxxxxx
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* === Contact info (green footer) === */}
+      <section className={sty.contactSection}>
+        <div className={sty.contactInner}>
+          {/* left big column */}
+          <div className={sty.contactHalf}>
+            <div className={sty.contactHalfInnerLeft}>
+              <div className={sty.contactLogoSide}>
+                <img
+                  src="/images/CAT_White.png"
+                  alt="CAT"
+                  className={sty.contactCatLogo}
+                />
+              </div>
+
+              <div className={sty.contactContentSide}>
+                <div className={sty.contactText}>
+                  <p>
+                    {t("landing_disclaimer_1")}{" "}
+                    <a
+                      href="https://inclusivespaces-heproject.eu/"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      InclusiveSpaces
+                    </a>{" "}
+                    {t("landing_disclaimer_2")}
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* right big column */}
+          <div className={sty.contactHalf}>
+            <div className={sty.contactHalfInnerRight}>
+              <div className={sty.contactContentSide}>
+                <div id="contact-info" className={sty.contactText}>
+                  <h3 className={sty.contactMiniTitle}>{t("landing_contact_title")}</h3>
+
+                  <div className={sty.contactList}>
+                    <div className={sty.contactLine}>
+                      <span className={sty.contactName}>{t("landing_contact_duran")}</span>
+                      <a className={sty.contactEmail} href="mailto:david.duran@tum.de">
+                        david.duran@tum.de
+                      </a>
+                    </div>
+
+                    <div className={sty.contactLine}>
+                      <span className={sty.contactName}>{t("landing_contact_buettner")}</span>
+                      <a className={sty.contactEmail} href="mailto:benjamin.buettner@tum.de">
+                        benjamin.buettner@tum.de
+                      </a>
+                    </div>
+
+                    <div className={sty.contactLine}>
+                      <span className={sty.contactName}>{t("landing_contact_zuckriegl")}</span>
+                      <a className={sty.contactEmail} href="mailto:lea.zuckriegl@tum.de">
+                        lea.zuckriegl@tum.de
+                      </a>
+                    </div>
+
+                    <div className={sty.contactLine}>
+                      <span className={sty.contactName}>{t("landing_contact_zuniga")}</span>
+                      <a className={sty.contactEmail} href="mailto:mariajose.zuniga@tum.de">
+                        mariajose.zuniga@tum.de
+                      </a>
+                    </div>
+
+                    <div className={sty.contactLine}>
+                      <span className={sty.contactName}>{t("landing_contact_huashu")}</span>
+                      <a className={sty.contactEmail} href="mailto:huashu.zhan@tum.de">
+                        huashu.zhan@tum.de
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className={sty.contactLogoSide}>
+                <div className={sty.partnerLogoColumn}>
+                  <img
+                    src="/images/logoIS_full.png"
+                    alt={t("logo_IS")}
+                    className={sty.partnerLogoImg}
+                  />
+                  <img
+                    src="/images/tum_logo_full.png"
+                    alt={t("logo_TUM")}
+                    className={sty.partnerLogoImg}
+                  />
+                  <img
+                    src="/images/logo_co-founded-eu_full.png"
+                    alt={t("logo_EU")}
+                    className={sty.partnerLogoImg}
+                  />
                 </div>
               </div>
             </div>
